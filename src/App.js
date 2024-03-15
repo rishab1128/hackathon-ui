2e756eb977bb3ff37187e26e03d9f97dd658dc33
rishab1128 <rishabpurkayastha28@gmail.com>
2024-03-15 17:37:43 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import OrganizerDashboard from "./pages/OraganizerDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="*" element={<LandingPage />} />
+        <Route path="/organiser/dashboard" element={<OrganizerDashboard />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid, Container } from "@mui/material";
+import LeftDrawer from "../components/LeftDrawer";
+// import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <>
+      <LeftDrawer />
+      <Container style={{ height: "100vh" }}>
+        <Grid
+          container
+          spacing={3}
+          justifyContent="center"
+          alignItems="center"
+          style={{ height: "100%" }}
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <Card style={{ height: "100%" }}>
+              <CardContent style={{ height: "100%" }}>
+                <Typography variant="h5" component="div" gutterBottom>
+                  Login as User
+                </Typography>
+                <Typography variant="body2">
+                  Click here to login as a user.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card style={{ height: "100%" }}>
+              <CardContent style={{ height: "100%" }}>
+                <Typography variant="h5" component="div" gutterBottom>
+                  Login as Organiser
+                </Typography>
+                <Typography variant="body2">
+                  Click here to login as an organiser.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card style={{ height: "100%" }}>
+              <CardContent style={{ height: "100%" }}>
+                <Typography variant="h5" component="div" gutterBottom>
+                  Login as Admin
+                </Typography>
+                <Typography variant="body2">
+                  Click here to login as an admin.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default LandingPage;
